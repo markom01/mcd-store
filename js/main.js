f3a843514f218e112c09e99b89c6c7ba60fb1181
80217109 <markomesa01@gmail.com>
2021-12-16 20:31:33 +0100

    Add one section and page
    index.html: Add Products section with cards and Footer.
    checkout.html: Add checkout form.

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Open off-canvas menu on click
+(function () {
+  'use strict'
+
+  document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  })
+})()
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
